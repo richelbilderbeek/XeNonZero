2c03f9212abe5c3f36baf4d3d0eaaf823e5a20f3
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-29 15:16:25 +0200

    'cargo run' shows file

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,18 @@ struct Cli {
 
 
 fn main() {
-  let args = Cli::from_args();
-  println!("XXXX");
-  println!("A {}", args.pattern);
-  println!("B {:?}", args.path);
-  println!("Hello, world!!!!");
+    let result = std::fs::read_to_string("/home/richel/GitHubs/xenon_zero/sprites.txt");
+    match result {
+        Ok(content) => { println!("File content: {}", content); }
+        Err(error) => { println!("Oh noes: {}", error); }
+    }
+
+    let content = std::fs::read_to_string("/home/richel/GitHubs/xenon_zero/sprites.txt")
+      .expect("could not read file from '/home/richel/GitHubs/xenon_zero/sprites.txt'");
+
+    println!("1111111");
+    for line in content.lines() {
+        println!("Another line: {}", line);
+    }
+    println!("Hello, world!!!!");
 }
