ebb192b9a8f9e69069a13eb7f0ac310303e6ab33
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-29 16:20:21 +0200

    My first trivial Rust function: 'create_str_of_spaces'

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,25 @@
-use std::io;
-
-fn old_main() {
-    let result = std::fs::read_to_string("/home/richel/GitHubs/xenon_zero/sprites.txt");
-    match result {
-        Ok(content) => { println!("File content: {}", content); }
-        Err(error) => { println!("Oh noes: {}", error); }
+fn create_str_of_spaces(n_chars: u8) -> String {
+    let mut s = String::new();
+    for number in 0..n_chars {
+      s.push(' ');
     }
+    return s;
+}
 
-    let content = std::fs::read_to_string("/home/richel/GitHubs/xenon_zero/sprites.txt")
-      .expect("could not read file from '/home/richel/GitHubs/xenon_zero/sprites.txt'");
-
-    println!("1111111");
-    for line in content.lines() {
-        println!("Another line: {}", line);
-    }
-    println!("Hello, world!!!!");
-
-    println!("Guess the number!");
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
+fn test() -> () {
+    assert!(create_str_of_spaces(0) == "".to_string());
+    assert!(create_str_of_spaces(1) == " ".to_string());
+    assert!(create_str_of_spaces(2) == "  ".to_string());
+    assert!(create_str_of_spaces(3) == "   ".to_string());
 }
 
 fn main() {
+    test();
 
-    let mut x = 10;
+    let x = 10;
     assert!(x == 10 , "x wasn't true!");
 
-    let mut s = String::new();
-    for number in 0..x {
-      s.push(' ');
-    }
+    let mut s = create_str_of_spaces(10);
     s.push('X');
     println!("........................................");
     println!("{}", s);
