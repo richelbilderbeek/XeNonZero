e8f1cdeafc6e9802bd7bd406cdca7e000b4d3047
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-29 20:25:36 +0200

    Create starfield

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
+
 fn create_str_of_spaces(n_chars: u8) -> String {
     let mut s = String::new();
-    for number in 0..n_chars {
+    for _i in 0..n_chars {
       s.push(' ');
     }
-    return s;
+    s
 }
+
+fn create_starfield(n_rows: usize) -> Vec<String> {
+  let mut v: Vec<String> = Vec::new();
+  v.resize(n_rows,  ". . . . . ".to_string());
+  v
+}
+
+
 
 fn test() -> () {
     assert!(create_str_of_spaces(0) == "".to_string());
     assert!(create_str_of_spaces(1) == " ".to_string());
     assert!(create_str_of_spaces(2) == "  ".to_string());
     assert!(create_str_of_spaces(3) == "   ".to_string());
+
+    assert!(8 == create_starfield(8).len());
 }
 
 fn main() {
@@ -27,13 +38,12 @@ fn main() {
 
 
 
-    //println!("Please input your guess.");
+    println!("Please enter a key (ASDWX) or 'help'.");
 
-    //let mut guess = String::new();
+    //let mut key = String::new();
+    //std::io::stdin().read_line(&mut key)
+    //  .expect("Please enter a key");
+    //println!("You pressed key: {}", key);
 
-    //io::stdin().read_line(&mut guess)
-    //    .expect("Failed to read line");
-
-    //println!("You guessed: {}", guess);
     println!("Got all the way to the end!");
 }
